b0c1c59082e5bccfff71ab8ff31fe2f8dc4c0cb8
markkenenberg <namnguyen.dev@gmail.com>
2015-03-17 18:25:33 +0700

    Sync basement

---
+++ Resource.sikuli/Resource.py
@@ -0,0 +1,9 @@
+imgOption = "1426324242544.png"
+imgSearch = "1426325554333.png"
+
+imgTuyChinh = "1426325722270.png"
+imgSwitchON = "1426325740621.png"
+imgSwitchOFF = "1426325754877.png"
+
+imgAmBaoOption = "1426325786357.png"
+imgPopUpOption = "1426325998812.png"
